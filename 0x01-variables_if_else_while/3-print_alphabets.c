1c2f3b7617f73e842f8993c7af235298e0a27b28
Mynameisothmane <Myemail@gmail.com>
2023-03-17 18:51:45 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter != '{')
+	{
+		putchar (letter);
+		letter++;
+	}
+	letter = 'A';
+	while (letter != '[')
+	{
+		putchar (letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
