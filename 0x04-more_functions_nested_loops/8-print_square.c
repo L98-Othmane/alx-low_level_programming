122bb0b0d42b59801f0e66afedd3250724367f46
My name <me@email.com>
2023-03-24 02:18:44 +0000

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_square - print a square of #'s given size
+  * @size: size to draw
+  */
+
+void print_square(int s)
+{
+	int r, c;
+
+	if (s > 0)
+	{
+		for (r = 1; r <= s; r++)
+		{
+			for (c = 1; c <= s; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
